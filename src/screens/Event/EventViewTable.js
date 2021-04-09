b71dacc2a69f467660e4c19932282351659095ee
jjchung3 <joejoonchung@gmail.com>
2021-04-09 09:40:59 -0700

    chore(): adding packages

--- src/screens/Event/EventViewTable.js
+++ src/screens/Event/EventViewTable.js
@@ -4,6 +4,12 @@ import { Route, Switch } from 'react-router-dom'
 
 const EventViewTable = props => {
 
+
+  /**
+   * grab the data from props
+   * use react-table here to render the table with the data
+   */
+
   return (
     <Flex>
     </Flex>
